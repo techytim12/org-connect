9dd5b533504d8ae50541946c11abcac6fec56700
Thomas Philip <thomasphilipchry@gmail.com>
2025-07-06 21:51:57 +0530

    Initial Project Commit

---
+++ src/main/java/com/org_connect/OrgConnectApplication.java
@@ -0,0 +1,13 @@
+package com.org_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org_connect/OrgConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
